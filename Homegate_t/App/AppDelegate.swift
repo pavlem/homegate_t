785dbfa62bd3c57d576bd3da1149c43bf06bf69b
Pavle Mijatovic <paul.mijatovic@sprintingsoftware.com>
2021-05-09 14:20:43 +0200

    Initial commit

---
+++ Homegate_t/App/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Homegate_t
+//
+//  Created by Pavle Mijatovic on 9.5.21..
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+//    var coordinator: MainCoordinator?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Homegate_tTests/Homegate_tTests.swift
@@ -0,0 +1,33 @@
+//
+//  Homegate_tTests.swift
+//  Homegate_tTests
+//
+//  Created by Pavle Mijatovic on 9.5.21..
+//
+
+import XCTest
+@testable import Homegate_t
+
+class Homegate_tTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
